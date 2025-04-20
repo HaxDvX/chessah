0647f037ceea19256acd7a9981c1b29fb37e087f
HaxDvX <158306954+HaxDvX@users.noreply.github.com>
2025-04-19 20:08:54 -0400

    Update chess.lua

--- chess.lua
+++ chess.lua
@@ -1,6 +1,1 @@
-local HttpService = game:GetService("HttpService")
-
-local url = "http://127.0.0.1:3000/custom"  -- Your local server endpoint
-local response = HttpService:GetAsync(url)  -- Sends GET request
-
-print(response)  -- Should print "Hello from Python!" if your server is up
+print("hii") -- Should print "Hello from Python!" if your server is up
